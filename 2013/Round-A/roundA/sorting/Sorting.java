fbcdfd665d0c83e17ddabb214c75d3668e01e069
renald25 <87197362+AldrenDG@users.noreply.github.com>
2021-08-30 20:16:20 -0400

    completed

--- 2013/Round-A/roundA/sorting/Sorting.java
+++ 2013/Round-A/roundA/sorting/Sorting.java
@@ -1,25 +1,60 @@
 package roundA.sorting;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
+import java.util.*;
+import java.io.*;
 
 
 public class Sorting{
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        String line ="";
-        while (scan.hasNextLine()) {
-            line = scan.nextLine();
-      
-        }
-        List<Integer> nums = new ArrayList<Integer>();
-        for(String s:line.split(" ")){
-        	nums.add(Integer.parseInt(s));
+		int testCase=0;
+		Scanner scan = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		testCase = Integer.parseInt(scan.nextLine());
+        
+		HashMap<Integer, String> strMap = new HashMap<Integer, String>();
+        for(int i=0;i<testCase;i++){
+        	int n=scan.nextInt();
+        	scan = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        	String line = scan.nextLine();
+        	
+        	List<Integer> evens = new ArrayList<Integer>();
+            List<Integer> odds = new ArrayList<Integer>();
+            
+            String[] arrStr = line.split(" ");
+
+            for(int cnt=0;cnt<n && cnt<arrStr.length;cnt++){
+            	int num = Integer.parseInt(arrStr[cnt]);
+            	if(num%2==0){
+            		evens.add(num);
+            	}else{
+            		odds.add(num);
+            	}
+            }
+            evens.sort(Comparator.reverseOrder());
+            odds.sort(Comparator.naturalOrder());
+            List<Integer> newOrders = new ArrayList<Integer>();
+            int e=0;
+            int o=0;
+            for(String s:line.split(" ")){
+            	int num = Integer.parseInt(s);
+            	if(num%2==0){
+            		newOrders.add(evens.get(e));
+            		e++;
+            	}else{
+            		newOrders.add(odds.get(o));
+            		o++;
+            	}
+            }
+            
+            strMap.put(i, newOrders.toString().replace("[", "").replace("]", ""));
         }
         
-       // nums.so
+        for(int i=0;i<testCase;i++){
+        	System.out.println("Case #"+(i+1)+": "+strMap.get(i));
+        }
+        
+        
 	}
 }
